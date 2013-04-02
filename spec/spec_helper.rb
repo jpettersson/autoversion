681f3b8df4522767c673abf749447cccef20c825
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-04-02 17:15:21 -0400

    cleaning up tmp folders after rspec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,8 @@ require_relative 'fixtures/versionfiles.rb'
 
 RSpec.configure do |config|
   # some (optional) config here
+  config.after(:all) do
+    FileUtils.rm_rf File.join(File.dirname(__FILE__), 'tmp', 'gitter')
+    FileUtils.rm_rf File.join(File.dirname(__FILE__), 'tmp', 'cli')
+  end
 end
