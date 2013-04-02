67f042e1d9ec2484d4a7cca37820f28c20fbc72c
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-04-02 01:27:12 -0400

    Added git automation options to the DSL

--- lib/autoversion/dsl.rb
+++ lib/autoversion/dsl.rb
@@ -1,14 +1,46 @@
 module Autoversion
   class DSL
 
+    class MissingReadBlock < Exception
+    end
+
+    class InvalidGitConfig < Exception
+    end
+
     attr_accessor :read_blk
     attr_accessor :write_blk
     attr_accessor :listeners
+    attr_accessor :config
 
     def initialize
       @read_blk = nil
       @write_blk = nil
       @listeners = []
+      @config = {
+        :git => {
+          :actions => [],
+          :prefix => '',
+          :stable_branch => 'master'
+        }
+      }
+    end
+
+    def validate!
+      # A read_version block is required
+      raise MissingReadBlock unless @read_blk
+      raise InvalidGitConfig if @config[:git][:actions].include?(:tag) && !@config[:git][:actions].include?(:commit)
+    end
+
+    def automate_git *args
+      if args.length == 0
+        @config[:git][:actions] = [:commit, :tag]
+      else
+        args[0].each do |arg| 
+          if [:actions, :stable_branch, :prefix].include?(arg[0])
+            @config[:git][arg[0]] = arg[1]
+          end
+        end
+      end
     end
 
     # Register a block that will be used to read the 
@@ -47,7 +79,9 @@ module Autoversion
       def evaluate(script)
         obj = self.new
         obj.instance_eval(script)
-        return obj.read_blk, obj.write_blk, obj.listeners
+        obj.validate!
+
+        return obj.read_blk, obj.write_blk, obj.listeners, obj.config
       end
     end
   end
